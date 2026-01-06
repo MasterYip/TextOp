d6ccf1f1a9684fec416ad21a5ea49cf8a93abff2
MasterYip-RCAMC4090 <2205929492@qq.com>
2026-01-06 12:22:11 +0800

    fix: body index mapping

--- TextOpTracker/scripts/data_dyeing/body_index_mapping.py
+++ TextOpTracker/scripts/data_dyeing/body_index_mapping.py
@@ -100,8 +100,8 @@ MOTIONCLIP_DOF_NAMES = [
 
 # IsaacLab ordering (alphabetical from URDF)
 # Note: This is the order returned by robot.data.body_pos_w, robot.data.body_quat_w, etc.
-ISAACLAB_BODY_NAMES = sorted(MOTIONCLIP_BODY_NAMES)
-ISAACLAB_DOF_NAMES = sorted(MOTIONCLIP_DOF_NAMES)
+ISAACLAB_BODY_NAMES = ['pelvis', 'left_hip_pitch_link', 'right_hip_pitch_link', 'waist_yaw_link', 'left_hip_roll_link', 'right_hip_roll_link', 'waist_roll_link', 'left_hip_yaw_link', 'right_hip_yaw_link', 'torso_link', 'left_knee_link', 'right_knee_link', 'left_shoulder_pitch_link', 'right_shoulder_pitch_link', 'left_ankle_pitch_link', 'right_ankle_pitch_link', 'left_shoulder_roll_link', 'right_shoulder_roll_link', 'left_ankle_roll_link', 'right_ankle_roll_link', 'left_shoulder_yaw_link', 'right_shoulder_yaw_link', 'left_elbow_link', 'right_elbow_link', 'left_wrist_roll_link', 'right_wrist_roll_link', 'left_wrist_pitch_link', 'right_wrist_pitch_link', 'left_wrist_yaw_link', 'right_wrist_yaw_link']
+ISAACLAB_DOF_NAMES = ['left_hip_pitch_joint', 'right_hip_pitch_joint', 'waist_yaw_joint', 'left_hip_roll_joint', 'right_hip_roll_joint', 'waist_roll_joint', 'left_hip_yaw_joint', 'right_hip_yaw_joint', 'waist_pitch_joint', 'left_knee_joint', 'right_knee_joint', 'left_shoulder_pitch_joint', 'right_shoulder_pitch_joint', 'left_ankle_pitch_joint', 'right_ankle_pitch_joint', 'left_shoulder_roll_joint', 'right_shoulder_roll_joint', 'left_ankle_roll_joint', 'right_ankle_roll_joint', 'left_shoulder_yaw_joint', 'right_shoulder_yaw_joint', 'left_elbow_joint', 'right_elbow_joint', 'left_wrist_roll_joint', 'right_wrist_roll_joint', 'left_wrist_pitch_joint', 'right_wrist_pitch_joint', 'left_wrist_yaw_joint', 'right_wrist_yaw_joint']
 
 
 def create_body_index_mapping():
